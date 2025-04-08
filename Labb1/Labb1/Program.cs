f772534ec5572d21091e9f4c36e83e8b7afd7125
isakstot <h23issto@du.se>
2025-04-08 18:19:08 +0200

    Ludwig

--- Labb1/Labb1/Program.cs
+++ Labb1/Labb1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Labb1
@@ -13,52 +14,43 @@ namespace Labb1
             int min = 0;
             int max = 10;
 
-            TimeSpan[] allTen = new TimeSpan[100];
-            TimeSpan[] allOneHundred = new TimeSpan[100];
-            TimeSpan[] allOneThousand = new TimeSpan[100];
-            TimeSpan[] allOneHundredThousand = new TimeSpan[100];
-            TimeSpan[] allOneMillion = new TimeSpan[100];
+            int[] arrSizes = [10, 100, 1000, 10000, 100000, 1000000];
+            TimeSpan[][] timeSpans = new TimeSpan[arrSizes.Length][];
+            int[][] randArr = new int[arrSizes.Length][];
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < arrSizes.Length; i++)
             {
-                int[] ten = CreateRandomArray(10, randNum, min, max);
-                int[] oneHundred = CreateRandomArray(100, randNum, min, max);
-                int[] oneThousand = CreateRandomArray(1000, randNum, min, max);
-                int[] oneHundredThousand = CreateRandomArray(100000, randNum, min, max);
-                int[] oneMillion = CreateRandomArray(1000000, randNum, min, max);
-
-                TimeSpan resultTen = CountNumber(ten, 1);
-                allTen[i] = resultTen;
-
-                TimeSpan resultOneHundred = CountNumber(oneHundred, 1);
-                allOneHundred[i] = resultOneHundred;
-
-                TimeSpan resultOneThousand = CountNumber(oneThousand, 1);
-                allOneThousand[i] = resultOneThousand;
-
-                TimeSpan resultOneHundredThousand = CountNumber(oneHundredThousand, 1);
-                allOneHundredThousand[i] = resultOneHundredThousand;
-
-                TimeSpan resultOneMillion = CountNumber(oneMillion, 1);
-                allOneMillion[i] = resultOneMillion;
+                randArr[i] = CreateRandomArray(arrSizes[i], randNum, min, max);
             }
 
-            double averageTen = allTen.Average(timeSpan => timeSpan.TotalMilliseconds);
-            double averageOneHundred = allOneHundred.Average(timeSpan => timeSpan.TotalMilliseconds);
-            double averageOneThousand = allOneThousand.Average(timeSpan => timeSpan.TotalMilliseconds);
-            double averageOneHundredThousand = allOneHundredThousand.Average(timeSpan => timeSpan.TotalMilliseconds);
-            double averageOneMillion = allOneMillion.Average(timeSpan => timeSpan.TotalMilliseconds);
+            for (int i = 0; i < arrSizes.Length; i++)
+            {
+                timeSpans[i] = new TimeSpan[100];
+                for (int j = 0; j < 100; j++)
+                {
+                    timeSpans[i][j] = CountNumber(randArr[i], 1);
+                }
 
-            Console.WriteLine($"Average time for 10: {averageTen}");
-            Console.WriteLine($"Average time for 100: {averageOneHundred}");
-            Console.WriteLine($"Average time for 1000: {averageOneThousand}");
-            Console.WriteLine($"Average time for 100000: {averageOneHundredThousand}");
-            Console.WriteLine($"Average time for 1000000: {averageOneMillion}");
+            }
+
+            double[] averageExecutionTime = new double[arrSizes.Length];
+            string[] lines = new string[arrSizes.Length +1];
+            lines[0] = "Data size;Average execution time (ms)";
+            string filePath = "stats.csv";
+            for (int i = 0; i < arrSizes.Length; i++)
+            {
+                averageExecutionTime[i] = Math.Round(timeSpans[i].Average(timeSpan => timeSpan.TotalMilliseconds), 5);
+                Console.WriteLine($"Average execution time for {arrSizes[i]}: {averageExecutionTime[i]}");
+                lines[i+1] = $"{arrSizes[i]};{averageExecutionTime[i]}";
+            }
+
+            File.WriteAllLines(filePath, lines);
+
         }
 
-        static int[] CreateRandomArray(int n, Random randNum, int min, int max)
+        static int[] CreateRandomArray(int sampleSize, Random randNum, int min, int max)
         {
-            int[] array = new int[n];
+            int[] array = new int[sampleSize];
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = randNum.Next(min, max);
@@ -70,7 +62,7 @@ namespace Labb1
         static TimeSpan CountNumber(int[] allNumbers, int desiredNumber)
         {
             var timer = new Stopwatch();
-            
+
             int count = 0;
             timer.Start();
             for (int i = 0; i < allNumbers.Length; i++)
@@ -82,7 +74,6 @@ namespace Labb1
             }
             timer.Stop();
             TimeSpan elapsed = timer.Elapsed;
-            //Console.WriteLine(count);
             return elapsed;
         }
     }
