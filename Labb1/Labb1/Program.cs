c84ed9691230e8c737d925244b27f7ad2e1d0410
isakstot <h23issto@du.se>
2025-04-11 15:55:08 +0200

    Final commit

--- Labb1/Labb1/Program.cs
+++ Labb1/Labb1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -9,13 +10,21 @@ namespace Labb1
     {
         static void Main(string[] args)
         {
-            // initialize a random number generator with a fixed seed
+            //Deluppgift 1
+            BigOTester(CountNumber, "countNumberStats.csv");
+            //Deluppgfit 2
+            BigOTester(BruteForceBiggestDifference, "bruteforceStats.csv");
+            BigOTester(BiggestDifference, "biggestDifferenceStats.csv");
+        }
+
+        static void BigOTester(Func<int[], double> method, string filePath)
+        {
             Random randNum = new Random(420);
             int min = 0;
             int max = 10;
 
-            int[] arrSizes = [10, 100, 1000, 10000, 100000, 1000000];
-            TimeSpan[][] timeSpans = new TimeSpan[arrSizes.Length][];
+            int[] arrSizes = [10, 1000, 10000, 100000, 1000000];
+            double[][] timeSpans = new double[arrSizes.Length][];
             int[][] randArr = new int[arrSizes.Length][];
 
             for (int i = 0; i < arrSizes.Length; i++)
@@ -25,27 +34,25 @@ namespace Labb1
 
             for (int i = 0; i < arrSizes.Length; i++)
             {
-                timeSpans[i] = new TimeSpan[100];
+                timeSpans[i] = new double[100];
                 for (int j = 0; j < 100; j++)
                 {
-                    timeSpans[i][j] = CountNumber(randArr[i], 1);
+                    timeSpans[i][j] = method(randArr[i]);
                 }
 
             }
 
             double[] averageExecutionTime = new double[arrSizes.Length];
-            string[] lines = new string[arrSizes.Length +1];
+            string[] lines = new string[arrSizes.Length + 1];
             lines[0] = "Data size;Average execution time (ms)";
-            string filePath = "stats.csv";
             for (int i = 0; i < arrSizes.Length; i++)
             {
-                averageExecutionTime[i] = Math.Round(timeSpans[i].Average(timeSpan => timeSpan.TotalMilliseconds), 5);
+                averageExecutionTime[i] = Math.Round(timeSpans[i].Average(), 5);
                 Console.WriteLine($"Average execution time for {arrSizes[i]}: {averageExecutionTime[i]}");
-                lines[i+1] = $"{arrSizes[i]};{averageExecutionTime[i]}";
+                lines[i + 1] = $"{arrSizes[i]};{averageExecutionTime[i]}";
             }
 
             File.WriteAllLines(filePath, lines);
-
         }
 
         static int[] CreateRandomArray(int sampleSize, Random randNum, int min, int max)
@@ -58,11 +65,11 @@ namespace Labb1
             return array;
         }
 
-        // O(n) time complexity
-        static TimeSpan CountNumber(int[] allNumbers, int desiredNumber)
+        // O(n)
+        static double CountNumber(int[] allNumbers)
         {
             var timer = new Stopwatch();
-
+            int desiredNumber = 1;
             int count = 0;
             timer.Start();
             for (int i = 0; i < allNumbers.Length; i++)
@@ -73,8 +80,52 @@ namespace Labb1
                 }
             }
             timer.Stop();
-            TimeSpan elapsed = timer.Elapsed;
-            return elapsed;
+
+            return timer.Elapsed.TotalMilliseconds;
+        }
+
+        // O(n^2)
+        static double BruteForceBiggestDifference(int[] arr)
+        {
+            var timer = new Stopwatch();
+            int difference = 0;
+
+            timer.Start();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] - arr[j] > difference)
+                        difference = arr[i] - arr[j];
+                }
+            }
+            timer.Stop();
+
+            return timer.Elapsed.TotalMilliseconds;
+        }
+
+        // O(n)
+        static double BiggestDifference(int[] arr)
+        {
+            var timer = new Stopwatch();
+            int max = 0;
+            int difference = 0;
+
+            timer.Start();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (difference < max - arr[i])
+                    difference = max - arr[i];
+            }
+            timer.Stop();
+
+            return timer.Elapsed.TotalMilliseconds;
         }
     }
 }
